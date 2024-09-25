0932e7bd285c9949e87fff04d4a41077492d419f
Cynario4eva <joshdela233@gmail.com>
2024-09-25 09:19:59 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+const enterBtn = document.getElementById('btn');
+
+
+
+const addBtn = ()=>{
+  if(!inputBox.value){
+    return alert('Please Enter Task ! ');
+  }else{
+    let li = document.createElement('li');
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span =document.createElement('span');
+    span.innerHTML = '\u00d7';
+    li.appendChild(span);
+  }
+  inputBox.value = '';
+  saveData()
+}
+
+listContainer.addEventListener('click',(e)=>{
+  if(e.target.tagName === ('LI')){
+    e.target.classList.toggle('checked');
+    saveData()
+  }
+  if(e.target.tagName === ('SPAN')){
+    e.target.parentElement.remove();
+    saveData()
+  }
+},false)
+
+
+const saveData = ()=>{
+  localStorage.setItem('data1',listContainer.innerHTML);
+}
+
+const showTask = ()=>{
+  listContainer.innerHTML = localStorage.getItem('data1')
+}
+showTask()
